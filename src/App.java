6ae0d0931d12cfc88025f50d0d8b174919b635c3
«Roman <research777@mail.ru>
2024-09-05 19:31:39 +0300

    iActor_+_Market

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import Classes.Actor;
+import Classes.Market;
+import Classes.OrdinaryClient;
+import Classes.SpecialClient;
+
+public class App {
+    public static void main(String[] args) {
+        Market market = new Market();
+
+        Actor client1 = new OrdinaryClient("Борис", 1);
+        Actor client2 = new OrdinaryClient("Ольга", 2);
+
+        Actor client3 = new SpecialClient("Киркоров", 1);
+
+        market.acceptToMarket(client1);
+        market.acceptToMarket(client2);
+        market.acceptToMarket(client3);
+
+        market.update();
+
+    }
+}
